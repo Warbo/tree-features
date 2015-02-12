39ad67908f8bfe24ccd57c4926846a32db33c916
Chris Warburton <chriswarbo@gmail.com>
2015-02-12 12:55:39 +0000

    Remove dependency on Emacs

--- default.nix
+++ default.nix
@@ -1,21 +1,10 @@
 with import <nixpkgs> {};
 
-coq_mtac = pkgs.stdenv.lib.overrideDerivation pkgs.coq (oldAttrs : {
-  name = "coq-mtac";
-  src  = pkgs.fetchgit {
-    url    = git://gitorious.org/mc-aixi-ctw/mc-aixi-ctw.git;
-    rev    = "2651fd3";
-    sha256 = "1949z7pjb51w89954narwcd1ykb9wxi7prldic1a1slxrr5b6lq7";
-  };
-});
-
 stdenv.mkDerivation {
   name = "generatinggoals";
   src  = ./.;
   buildInputs = [
     coq_mtac
-    pkgs.emacs
-    pkgs.emacs24Packages.proofgeneral
     pkgs.haskellPackages.ghc
     pkgs.haskellPackages.xml
     pkgs.haskellPackages.QuickCheck
