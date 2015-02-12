0a3d9b05827ec007c23b727d7610cb02f4e26748
Chris Warburton <chriswarbo@gmail.com>
2015-02-12 00:02:41 +0000

    Added coq_mtac

--- default.nix
+++ default.nix
@@ -1,5 +1,14 @@
 with import <nixpkgs> {};
 
+coq_mtac = pkgs.stdenv.lib.overrideDerivation pkgs.coq (oldAttrs : {
+  name = "coq-mtac";
+  src  = pkgs.fetchgit {
+    url    = git://gitorious.org/mc-aixi-ctw/mc-aixi-ctw.git;
+    rev    = "2651fd3";
+    sha256 = "1949z7pjb51w89954narwcd1ykb9wxi7prldic1a1slxrr5b6lq7";
+  };
+});
+
 stdenv.mkDerivation {
   name = "generatinggoals";
   src  = ./.;
