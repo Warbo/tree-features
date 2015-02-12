f772bffac31e54b19d64dba373e90ca22d7121e6
Chris Warburton <chriswarbo@gmail.com>
2015-02-12 12:56:19 +0000

    Read bits from argv

--- processor.hs
+++ processor.hs
@@ -1,8 +1,18 @@
 import Features
+import System.Environment
 import Text.XML.Light.Input
 import Text.XML.Light.Types
 import XmlHelper
 
-bits = 31
+getBits :: (Integral a, Read a) => IO a
+getBits = do args <- getArgs
+             return $ case (map read args) of
+                           []    -> defaultBits
+                           (n:_) -> n
 
-main = fmap (parseRequest bits . parse) getContents >>= print
+defaultBits :: (Integral a) => a
+defaultBits = 31
+
+main = do bits  <- getBits
+          input <- getContents
+          print (parseRequest bits (parse input))
