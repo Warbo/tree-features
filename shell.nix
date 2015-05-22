5a9ba3cc5b882a84d6610d6f94fa61b0a4dadb85
Chris Warburton <chriswarbo@gmail.com>
2015-05-23 00:21:45 +0100

    Update cabal file

--- shell.nix
+++ shell.nix
@@ -1,3 +1,19 @@
-with import <nixpkgs> {};
-with haskellPackages;
-callPackage ./. {}
+with (import <nixpkgs> {}).pkgs;
+let pkg = haskellngPackages.callPackage
+            ({ mkDerivation, base, directory, MissingH, parsec, QuickCheck
+             , stdenv, xml
+             }:
+             mkDerivation {
+               pname = "TreeFeatures";
+               version = "0.1.0.0";
+               src = ./.;
+               isLibrary = false;
+               isExecutable = true;
+               buildDepends = [ base MissingH parsec xml ];
+               testDepends = [ base directory MissingH parsec QuickCheck xml ];
+               homepage = "http://chriswarbo.net/essays/repos/tree-features.html";
+               description = "Feature extraction for tree structured data";
+               license = stdenv.lib.licenses.gpl3;
+             }) {};
+in
+  pkg.env
